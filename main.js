c97aca60addb02d9069a36fbe6c46fc2276296d7
Kro <124914389+Krowno04@users.noreply.github.com>
2023-03-23 14:05:02 -0400

    Added what I have so far

--- main.js
+++ main.js
@@ -1,44 +1,63 @@
 //
-//  JS File
-//  You may remove the code below - it's just boilerplate
+// APP VARIABLES AND DOM ELEMENTS
 //
 
-//
-// Variables
-//
-
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
-
-// Variables
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
+// Array of pokemons that are provided by the user
+const ToDoOngoing = [];
+// The ul for the list of pokemon
+const ToDoList = document.querySelector(".Ongoing-items");
+// The form where we intake pokemon
+const TodoForm = document.querySelector(".add-item");
+// The form text element that has the name the user provided
+const ToDoName = document.querySelector("#item-name");
 
 //
-// Functions
+// FUNCTIONS
 //
 
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
+// Handle the event when a user submits the form
+function addPokemonItem(e) {
+  // Stop browser default form submission
+  e.preventDefault();
+  // Get the text from the input field
+  const ToDoItemName = ToDoName.value;
+  // Add the user defined pokemon to our array
+  ToDoOngoing.push(ToDoItemName);
+  // Draw the list of pokemons
+  renderList(ToDoOngoing, ToDoList);
+  // Reset the form so that the text field name is cleared
+  TodoForm.reset();
+}
+
+// Draw the list of items
+// Step 1: remove all of the children in the UL list
+// Step 2: for each entry in the array add the array item to the list
+function renderList(items, itemsList) {
+  // Clear all of the entries in the list
+  while (itemsList.firstChild) {
+    itemsList.removeChild(itemsList.firstChild);
   }
 
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
+  // For each item in the list add a list item
+  for (let i = 0; i < items.length; i++) {
+    // Create the list item and add the text
+    const listItem = document.createElement("li");
+    listItem.textContent = items[i];
+    // On the last item in the list, add the annimation class
+    if (i === items.length - 1) {
+      listItem.classList.add("new-item-annimate");
+    }
+    // Add the list item to the list
+    itemsList.appendChild(listItem);
+  }
 }
 
 //
-// Inits & Event Listeners
+// EVENT LISTENERS AND INITIALISION
 //
 
-inititialise();
+// Add the submit form handler
+TodoForm.addEventListener("submit", addPokemonItem);
+
+// Draw the list
+renderList(ToDoOngoing, ToDoList);
